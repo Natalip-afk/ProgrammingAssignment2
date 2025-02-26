e992d6459cff93283623fa11c31216ad4b6ffc74
Natalip-afk <naperezrivas@gmail.com>
2025-02-26 14:23:31 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,13 @@
 ## functions do
 
 ## Write a short comment describing this function
+# Esta función crea un objeto especial que puede almacenar una matriz y su inversa en caché. 
+# Aquí está el desglose de las partes de la función:
+# inv es una variable que se inicializa como NULL. Esta variable almacenará la inversa de la matriz.
+# set(y) es una función que asigna la matriz y a la variable x y resetea la variable inv a NULL (esto es útil si se cambia la matriz original).
+# get() es una función que devuelve la matriz almacenada en x.
+# setInverse(inverse) es una función que asigna la inversa inverse a la variable inv.
+# getInverse() es una función que devuelve la inversa almacenada en inv.
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -18,9 +25,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
-
 ## Write a short comment describing this function
+# El proposito de esta funcion es calcular la inversa de la matriz y almacenar el resultado en caché 
+# A partir de un objeto especal creado por la función makeCacheMatrix
+
 
 cacheSolve <- function(x, ...) {
     inv <- x$getInverse()
